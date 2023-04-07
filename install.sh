6a9b4229310f14b4a1c13f3faeb7160d20b4bcb4
Zhaoxi Chen <FrozenBurning@users.noreply.huggingface.co>
2023-04-07 13:44:16 +0000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,20 @@
+cd SceneDreamer
+CURRENT=$(pwd)
+for p in correlation channelnorm resample2d bias_act upfirdn2d; do
+    cd imaginaire/third_party/${p};
+    rm -rf build dist *info;
+    python setup.py install;
+    cd ${CURRENT};
+done
+
+for p in gancraft/voxlib; do
+  cd imaginaire/model_utils/${p};
+  make all
+  cd ${CURRENT};
+done
+
+cd gridencoder
+python setup.py build_ext --inplace
+python -m pip install .
+cd ${CURRENT}
+cd ..
