429a0453d6e4648cbb2ebfd139e8706cca3596a3
Zhaoxi Chen <FrozenBurning@users.noreply.huggingface.co>
2023-04-07 14:44:18 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 export CUDA_VERSION=$(nvcc --version| grep -Po "(\d+\.)+\d+" | head -1)
 CURRENT=$(pwd)
-for p in correlation channelnorm resample2d bias_act upfirdn2d; do
+for p in upfirdn2d; do
     cd imaginaire/third_party/${p};
     rm -rf build dist *info;
     python setup.py install;
+    python -m pip install .
     cd ${CURRENT};
 done
 
